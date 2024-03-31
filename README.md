921d96eb84c4979e47178e04eb225e1153dfd03e
unknown <pradipta.ss2004@gmail.com>
2024-03-31 12:05:25 +0530

    template & requirements added

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Inclusive-Dialogue-Detector
+
+## How to run ?
+
+```bash 
+conda create -n hate python=3.8 -y 
+```
+
+```bash
+conda activate hate 
+```
+
+```bash
+pip install -r requirements.txt 
+```
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+setup(
+   name="Inclusive-Dialogue-Detector",
+   version="0.0.1",
+   author="Pradipta",
+   author_email="sahoo.pradipta236@gmail.com",
+   packages=find_packages(),
+   install_requires=[]
+
+)
